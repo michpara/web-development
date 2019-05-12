dcb6f817907572390a09e80a59ae11a6145f2e61
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-05-12 14:33:35 -0400

    alignement

--- SQL Zoo/Q2.sql
+++ SQL Zoo/Q2.sql
@@ -2,94 +2,78 @@
 
 #Question 1
 SELECT name 
-FROM world
-WHERE name 
-LIKE 'Y%';
+  FROM world
+ WHERE name LIKE 'Y%';
 
 #Question 2
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%y';
+  FROM world
+ WHERE name LIKE '%y';
 
 #Question 3
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%x%';
+  FROM world
+ WHERE name LIKE '%x%';
 
 #Question 4
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%land';
+  FROM world
+ WHERE name LIKE '%land';
 
 #Question 5
 SELECT name 
-FROM world
-WHERE name 
-LIKE 'C%' 
-AND name 
-LIKE '%ia';
+  FROM world
+ WHERE name LIKE 'C%' 
+   AND name LIKE '%ia';
 
 #Question 6
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%oo%';
+  FROM world
+ WHERE name LIKE '%oo%';
 
 #Question 7
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%a%a%a%';
+  FROM world
+ WHERE name LIKE '%a%a%a%';
 
 #Question 8
 SELECT name
-FROM world
-WHERE name 
-LIKE '_t%'
-ORDER BY name;
+  FROM world
+ WHERE name LIKE '_t%'
+ ORDER BY name;
 
 #Question 9
 SELECT name 
-FROM world
-WHERE name 
-LIKE '%o__o%';
+  FROM world
+ WHERE name LIKE '%o__o%';
 
 #Question 10
 SELECT name 
-FROM world
-WHERE name
-LIKE '____';
+  FROM world
+ WHERE name LIKE '____';
 
 #Question 11
 SELECT name
-FROM world
-WHERE name = capital;
+  FROM world
+ WHERE name = capital;
 
 #Question 12
 SELECT name
-FROM world
-WHERE capital 
-LIKE '%city';
+  FROM world
+ WHERE capital LIKE '%city';
 
 #Question 13
 SELECT capital, name 
-FROM world 
-WHERE capital 
-LIKE CONCAT('%', name,'%');
+  FROM world 
+ WHERE capital LIKE CONCAT('%', name,'%');
 
 #Question 14
 SELECT capital, name 
-FROM world 
-WHERE capital 
-LIKE CONCAT('%', name, '%') 
-AND name != capital;
+  FROM world 
+ WHERE capital LIKE CONCAT('%', name, '%') 
+   AND name != capital;
 
 #Question 15
-SELECT name, R
-EPLACE(capital, name, '') AS extension 
-FROM world 
-WHERE capital
-LIKE CONCAT(name, '_%');
+SELECT name, REPLACE(capital, name, '') AS extension 
+  FROM world 
+ WHERE capital LIKE CONCAT(name, '_%');

a269f3a77dbb25455751df939eeed4f29bf6b3ea
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-05-16 22:02:15 -0400

    finished Q7

--- SQL Zoo/Q7.sql
+++ SQL Zoo/Q7.sql
@@ -97,9 +97,54 @@ HAVING COUNT(title)=(SELECT MAX(c)
 );
 
 #Question 12
+SELECT title, name 
+  FROM movie 
+  JOIN casting 
+    ON (movieid = movie.id AND ord =1) 
+  JOIN actor 
+    ON (actorid = actor.id) 
+ WHERE movie.id 
+    IN (SELECT movieid FROM casting WHERE actorid IN (SELECT id FROM actor WHERE name = 'Julie Andrews'));
+
 #Question 13
+SELECT name 
+  FROM actor 
+  JOIN casting 
+    ON casting.actorid = actor.id 
+ WHERE ord =1 
+ GROUP BY name 
+HAVING COUNT(ord) >= 30;
+
 #Question 14
+SELECT title, COUNT(actorid) 
+  FROM movie 
+  JOIN casting 
+    ON movieid = movie.id
+  JOIN actor 
+    ON actorid = actor.id 
+  WHERE yr = 1978 
+  GROUP BY title 
+  ORDER BY COUNT(name) DESC, title;
+
 #Question 15
+SELECT name 
+ FROM actor 
+ JOIN casting 
+   ON actorid = actor.id 
+ JOIN movie 
+   ON movieid = movie.id 
+WHERE movieid 
+   IN (SELECT movieid FROM casting WHERE actorid IN (SELECT id FROM actor WHERE name = 'Art Garfunkel')) 
+  AND name != 'Art Garfunkel';
+
+#Quiz
+1. C
+2. E
+3. C
+4. B
+5. D
+6. C
+7. B
 
 
 

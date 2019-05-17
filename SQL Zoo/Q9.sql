5408e46da50704d142f577ab34e25f85984c0b9d
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-05-17 16:16:28 -0400

    alignment

--- SQL Zoo/Q9.sql
+++ SQL Zoo/Q9.sql
@@ -10,42 +10,68 @@ SELECT id
  WHERE name = 'Craiglockhart';
 
 #Question 3
-SELECT id, name FROM stops JOIN route ON route.stop = stops.id WHERE num = 4 and company = 'LRT';
+SELECT id, name 
+  FROM stops 
+  JOIN route ON route.stop = stops.id 
+ WHERE num = 4 
+   AND company = 'LRT';
 
 #Question 4
-SELECT company, num, COUNT(*)
-FROM route WHERE stop=149 OR stop=53
+  SELECT company, num, COUNT(*)
+    FROM route 
+   WHERE stop=149 
+      OR stop=53
 GROUP BY company, num
-HAVING COUNT(*) = 2;
+  HAVING COUNT(*) = 2;
 
 #Question 5
 SELECT a.company, a.num, a.stop, b.stop
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-WHERE a.stop=53 and b.stop = 149;
-
+  FROM route a 
+  JOIN route b ON (a.company=b.company AND a.num=b.num)
+ WHERE a.stop=53 
+   AND b.stop = 149;
 
 #Question 7
 SELECT a.company, a.num, stopa.name, stopb.name
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
+  FROM route a 
+  JOIN route b ON (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart' and stopb.name = 'London Road';
-
-
-#Question 8
-SELECT a.company, a.num FROM route a JOIN route b ON a.num = b.num AND a.company = b.company JOIN stops stopa ON (a.stop=stopa.id) JOIN stops stopb ON (b.stop=stopb.id) WHERE stopa.name = 'Haymarket' AND stopb.name = 'Leith' GROUP BY a.company, a.num;
-
-
+ WHERE stopa.name='Craiglockhart' 
+   AND stopb.name = 'London Road';
 
 #Question 8
-SELECT a.company, a.num FROM route a JOIN route b ON a.num = b.num AND a.company = b.company JOIN stops stopa ON (a.stop=stopa.id) JOIN stops stopb ON (b.stop=stopb.id) WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross' GROUP BY a.company, a.num;
+  SELECT a.company, a.num 
+    FROM route a 
+    JOIN route b ON a.num = b.num 
+     AND a.company = b.company 
+    JOIN stops stopa ON (a.stop=stopa.id) 
+    JOIN stops stopb ON (b.stop=stopb.id) 
+   WHERE stopa.name = 'Haymarket' 
+     AND stopb.name = 'Leith' 
+GROUP BY a.company, a.num;
 
+#Question 8
+  SELECT a.company, a.num 
+    FROM route a 
+    JOIN route b ON a.num = b.num 
+     AND a.company = b.company 
+    JOIN stops stopa ON (a.stop=stopa.id) 
+    JOIN stops stopb ON (b.stop=stopb.id) 
+   WHERE stopa.name = 'Craiglockhart' 
+     AND stopb.name = 'Tollcross' 
+GROUP BY a.company, a.num;
 
 #Question 9
-SELECT DISTINCT stopa.name, a.company, a.num FROM route a JOIN route b ON a.num = b.num AND a.company = b.company JOIN stops stopa ON stopa.id = a.stop JOIN stops stopb ON stopb.id = b.stop WHERE stopb.name = 'Craiglockhart';
+SELECT DISTINCT stopa.name, a.company, a.num 
+  FROM route a 
+  JOIN route b ON a.num = b.num 
+   AND a.company = b.company 
+  JOIN stops stopa ON stopa.id = a.stop 
+  JOIN stops stopb ON stopb.id = b.stop 
+ WHERE stopb.name = 'Craiglockhart';
 
+#Question 10
 
 
 

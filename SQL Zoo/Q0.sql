f28e79e9df8f37accebe0940acaf36317bd9e51b
Michelle Paradis <46390671+michpara@users.noreply.github.com>
2019-05-17 11:53:04 -0400

    alignment

--- SQL Zoo/Q0.sql
+++ SQL Zoo/Q0.sql
@@ -2,19 +2,19 @@
 
 #Question 1
 SELECT population 
-FROM world 
-WHERE name = 'Germany';
+  FROM world 
+ WHERE name = 'Germany';
 
 #Question 2
 SELECT name, population 
-FROM world
-WHERE name 
-IN ('Sweden', 'Norway', 'Denmark');
+  FROM world
+ WHERE name 
+    IN ('Sweden', 'Norway', 'Denmark');
 
 #Question 3
-SELECT name, area 
-FROM world
-WHERE area 
+ SELECT name, area 
+   FROM world
+  WHERE area 
 BETWEEN 200000 AND 250000;
 
 #Quiz
